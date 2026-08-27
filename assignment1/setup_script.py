cfc9b5e7db7e00ce7f67680989c7f4c9520772c6
Ivan <ivanhinojos9@gmail.com>
2026-08-26 19:51:55 -0600

    assignment1

---
+++ assignment1/setup_script.py
@@ -0,0 +1,13 @@
+# %%
+import platform
+
+print(f"I am running on Google's servers: {platform.node()}")
+
+# %% [markdown]
+# # Local Test Success
+
+# %%
+name = input("Enter your name: ") 
+print(f"Happy coding, {name}!")
+
+
